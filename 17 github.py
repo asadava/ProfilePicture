7d1fd75e9c597d29e3f928efcce5c6e4bbfa6777
asadava <65477758+asadava@users.noreply.github.com>
2021-09-16 12:34:42 -0500

    The code in my profile picture, in python form!

---
+++ 17 github.py
@@ -0,0 +1,13 @@
+import time
+print("asadava\'s github page is cool")
+time.sleep(2)
+isit = input("Is it?")
+time.sleep(2)
+no = ("no")
+print("We heard you loud and clear.")
+time.sleep(2)
+if isit == no:
+    print("You\'re wrong.")
+else:
+    print("You\'re correct.")
+time.sleep(2)
